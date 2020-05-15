b315fa2eb2b64a70afb004f04c5a057f8d984cf0
JavierCastellD <javicastelldiaz@gmail.com>
2020-05-15 19:26:13 +0200

    Cambios para generar modelos VotingClassifier

--- src/machine_learning/persistModelo.py
+++ src/machine_learning/persistModelo.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
 from joblib import dump
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, VotingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from imblearn.under_sampling import RandomUnderSampler
 from src.machine_learning.predictorcodoninicio import featureSelection
@@ -40,10 +40,10 @@ def aplicarTransformaciones(X_train, y_train):
 
 # Lectura del fichero
 X = pd.read_csv('/home/javi/Desktop/PredictorMutacionCodonInicio/data/entrada/homo_sapiens_filtered.tsv', sep='\t')
-name = 'DT5_2'
+name = 'VC18'
 
-# Valores de configuraciçon
-n = 6
+# Valores de configuracion
+n = 3
 us = 0.3
 RANDOM_STATE = 1234
 
@@ -71,10 +71,23 @@ X_trans, y_trans, ct = aplicarTransformaciones(X_sel, y_res)
 # Creamos el modelo
 #clf = ExtraTreesClassifier(random_state=RANDOM_STATE, class_weight='balanced', bootstrap=False, max_depth=16,
 #                           min_samples_leaf=1, min_samples_split=2, n_estimators=200)
-clf = DecisionTreeClassifier(random_state=RANDOM_STATE, max_depth=None, min_samples_leaf=1,
-                             min_samples_split=10, criterion='gini')
+#clf = DecisionTreeClassifier(random_state=RANDOM_STATE, max_depth=None, min_samples_leaf=1,
+#                             min_samples_split=10, criterion='gini')
 #clf = RandomForestClassifier(bootstrap=False, max_depth=16, min_samples_leaf=2, min_samples_split=10,
 #                             n_estimators=20, class_weight='balanced')
+dt1 = DecisionTreeClassifier(max_depth=16, min_samples_leaf=1, min_samples_split=10, criterion='entropy',
+                             random_state=RANDOM_STATE)
+dt2 = DecisionTreeClassifier(max_depth=None, min_samples_leaf=1, min_samples_split=10, criterion='gini',
+                             random_state=RANDOM_STATE)
+rfc1 = RandomForestClassifier(bootstrap=False, max_depth=32, min_samples_leaf=2, min_samples_split=2,
+                              class_weight='balanced', n_estimators=50, random_state=RANDOM_STATE)
+rfc2 = RandomForestClassifier(bootstrap=False, max_depth=16, min_samples_leaf=2, min_samples_split=10,
+                              class_weight='balanced', n_estimators=20, random_state=RANDOM_STATE)
+et8 = ExtraTreesClassifier(bootstrap=False, max_depth=16, min_samples_leaf=1, min_samples_split=2,
+                           n_estimators=200, class_weight='balanced', random_state=RANDOM_STATE)
+
+clf = VotingClassifier(estimators=[('DT5.1',dt1),('RFC1.1',rfc1),('RFC1.2',rfc2)])
+
 
 # Entrenamos el modelo
 clf.fit(X_trans, y_trans)
